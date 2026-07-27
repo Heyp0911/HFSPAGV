7d3d3aced66b9e622b577120b4186c6125b7d607
Heyp0911 <17720902580@163.com>
2026-07-27 23:20:11 +0800

    更新hfsp

--- hfsp.py
+++ hfsp.py
@@ -4,6 +4,7 @@ from pptx.enum.shapes import MSO_SHAPE
 from pptx.dml.color import RGBColor
 
 # 定义原图中的主题颜色
+# 定义原图中的主题颜色111
 def create_hfsp_diagram():
     # 创建一个新的PPT，选择一张空白幻灯片 (布局索引为6)
     prs = Presentation()
